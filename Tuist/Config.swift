ce671ed589cec1ea91543c8c74e4ab603d984472
Byung jin <bjcho1503@naver.com>
2023-08-26 15:19:25 +0900

    tuist WorkSpace 작성

--- Tuist/Config.swift
+++ Tuist/Config.swift
@@ -1,7 +1,3 @@
 import ProjectDescription
 
-let config = Config(
-    plugins: [
-        .local(path: .relativeToManifest("../../Plugins/TuistPractice")),
-    ]
-)
+let config = Config()
---
+++ WorkSpace.swift
@@ -0,0 +1,10 @@
+import ProjectDescription
+import ProjectDescriptionHelpers
+
+let workSpace = Workspace(
+    name: "TuistPractice",
+    projects: [
+        "Projects/**"
+    ],
+    fileHeaderTemplate: nil
+)
